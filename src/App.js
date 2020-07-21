d7525d3f9c8b166c5703ba174520c85fce3d7380
Theo Dao <trungdao912@gmail.com>
2020-07-21 14:12:14 +0700

    clean up unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import('./Warning'));
-
 class App extends Component {
-  state = {
-    count: 0,
-  };
-
   render() {
     return (
       <>
         <h1>Hello world !</h1>
-        <h2>Count: {this.state.count}</h2>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Plus
-        </button>
-        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
-          Minus
-        </button>
-        {this.state.count > 6 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
       </>
     );
   }
--- src/Warning.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Warning extends React.Component {
-  render() {
-    return <>Warning</>;
-  }
-}
